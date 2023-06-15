4a3e462f961978ad2092b003c0020d12e3957f22
yulissabalvuca <135991103+yulissabalvuca@users.noreply.github.com>
2023-06-15 09:37:53 -0500

    Add files via upload

---
+++ matematicas.py
@@ -0,0 +1,9 @@
+def sumar(a,b):
+    return a +b 
+def restar (a,b):
+    return a -b 
+
+def multiplicacion (a,b):
+    return a*b 
+def division (a,b):
+    return a/b 
---
+++ programa.py
@@ -0,0 +1,8 @@
+from matematicas import sumar, restar, multiplicacion,division
+from algebra import distancia
+
+print(sumar(20,12))
+print(restar(40,22))
+print (multiplicacion(3,4))
+print (division (10,2))
+print(distancia (1,2,3,4))
